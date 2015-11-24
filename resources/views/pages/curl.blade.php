a9eeab789d42a187e969e22d88c01de3ddc794d6
Michael Hopkins <mhopkins321@gmail.com>
2015-11-23 19:34:11 -0700

    formatting

--- resources/views/pages/curl.blade.php
+++ resources/views/pages/curl.blade.php
@@ -1,5 +1,5 @@
 <code>
-    curl -X POST -H "Content-Type: multipart/form-data" \
+curl -X POST -H "Content-Type: multipart/form-data" \
     -F "xml=\full\path\to\xmlFile.xml" \
     -F "namespaces=namespace1,namespace2,name3" \
     'http://xml2jsonapi.com'

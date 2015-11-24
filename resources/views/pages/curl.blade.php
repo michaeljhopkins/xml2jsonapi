a1436ba84f02f11b590d0048dec97acc30c87df9
Michael Hopkins <mhopkins321@gmail.com>
2015-11-23 19:03:04 -0700

    adding examples and such

---
+++ resources/views/pages/curl.blade.php
@@ -0,0 +1,6 @@
+<code>
+    curl -X POST -H "Content-Type: multipart/form-data" \
+    -F "xml=\full\path\to\xmlFile.xml" \
+    -F "namespaces=namespace1,namespace2,name3" \
+    'http://xml2jsonapi.com'
+</code>
---
+++ resources/views/pages/postman.blade.php
@@ -0,0 +1,45 @@
+<code>
+    {
+    "id": "0d05343f-b0a7-a021-04c7-a390a930ff9f",
+    "name": "xml2josnapi",
+    "description": "",
+    "order": [""],
+    "folders": [],
+    "timestamp": 1448154413564,
+    "owner": "",
+    "remoteLink": "",
+    "public": false,
+    "requests": [
+    {
+    "id": "683b06e8-845d-4fdc-9cf2-b1f45a053d1b",
+    "headers": "",
+    "url": "http://xml2jsonapi.com",
+    "preRequestScript": "",
+    "pathVariables": {},
+    "method": "POST",
+    "data": [
+    {
+    "key": "xml",
+    "value": "xmlFile.xml",
+    "type": "file",
+    "enabled": true
+    },{
+    "key": "namespaces",
+    "value": "namespace1,namespace2,name3",
+    "type": "text",
+    "enabled": true
+    }
+    ],
+    "dataMode": "params",
+    "version": 2,
+    "tests": "",
+    "currentHelper": "normal",
+    "helperAttributes": {},
+    "time": 1448329837783,
+    "name": "http://xml2jsonapi.com",
+    "description": "Example of how to use Xml2jsonapi.com via Postman request",
+    "collectionId": "0d05343f-b0a7-a021-04c7-a390a930ff9f"
+    }
+    ]
+    }
+</code>
